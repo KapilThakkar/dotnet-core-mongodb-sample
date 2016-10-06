01aefddc46fb8c5dc34e90d4185ae7d5dfa7243e
kapilthakkar <kapil@click2cloud.net>
2016-10-06 18:44:43 +0530

    code added

---
+++ src/dotnet-core-sample/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+/* ------------------------------------------------------------------- *
+*                            Click2Cloud Inc.                          *
+*                  Copyright ©2016 - All Rights reserved               *
+*                                                                      *
+* Apache 2.0 License                                                   *
+* You may obtain a copy of the License at                              * 
+* http://www.apache.org/licenses/LICENSE-2.0                           *
+* Unless required by applicable law or agreed to in writing,           *
+* software distributed under the License is distributed on an "AS IS"  *
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  *
+* or implied. See the License for the specific language governing      *
+* permissions and limitations under the License.                       *
+*                                                                      *
+* -------------------------------------------------------------------  */
+#endregion Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Click2Cloud.Samples.AspNetCore.MvcMongoDb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/dotnet-core-sample/Startup.cs
@@ -0,0 +1,77 @@
+﻿#region Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+/* ------------------------------------------------------------------- *
+*                            Click2Cloud Inc.                          *
+*                  Copyright ©2016 - All Rights reserved               *
+*                                                                      *
+* Apache 2.0 License                                                   *
+* You may obtain a copy of the License at                              * 
+* http://www.apache.org/licenses/LICENSE-2.0                           *
+* Unless required by applicable law or agreed to in writing,           *
+* software distributed under the License is distributed on an "AS IS"  *
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  *
+* or implied. See the License for the specific language governing      *
+* permissions and limitations under the License.                       *
+*                                                                      *
+* -------------------------------------------------------------------  */
+#endregion Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Click2Cloud.Samples.AspNetCore.MvcMongoDb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
